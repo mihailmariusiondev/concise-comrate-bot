2128de89b7fb4a8a81addbfb23e0c1ce90afdef7
Marius <marius.mihail@enzo.es>
2023-07-31 21:34:49 +0200

    works for now

--- bot.js
+++ bot.js
@@ -37,41 +37,118 @@ var __generator = (this && this.__generator) || function (thisArg, body) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 var telegraf_1 = require("telegraf");
+var openai_1 = require("openai");
 var BOT_TOKEN = "6610691916:AAHAVageSJ5M7wSb4PRZozgJmVcCTVWYaQk";
-var GPT_API_ENDPOINT = "https://api.openai.com/v1/engines/davinci/completions";
-var GPT_API_KEY = "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";
+var GPT_API_KEY = "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";
+var configuration = new openai_1.Configuration({
+    apiKey: GPT_API_KEY,
+});
+var openai = new openai_1.OpenAIApi(configuration);
 var bot = new telegraf_1.Telegraf(BOT_TOKEN);
+// A simple in-memory storage for the recent messages.
+// For a production environment, consider using a more persistent storage solution.
+var recentMessages = {};
 bot.start(function (ctx) { return ctx.reply("Welcome to the Summary Bot!"); });
-var lastMessage = "";
-bot.on("message", function (ctx) { return __awaiter(void 0, void 0, void 0, function () {
-    var _a, _b, _c;
-    return __generator(this, function (_d) {
-        if ("text" in ctx.message && ctx.message.text) {
-            // If the command is '/summarize', then summarize the difference
-            if ((_b = (_a = ctx.message) === null || _a === void 0 ? void 0 : _a.text) === null || _b === void 0 ? void 0 : _b.startsWith("/summarize")) {
-                console.log("Message to be summarized:", ctx.message); // <-- Log the message to be summarized
-                // const response = await axios.post(
-                //   GPT_API_ENDPOINT,
-                //   {
-                //     prompt: `Summarize: ${lastMessage}`,
-                //     max_tokens: 150, // Or another limit you prefer
-                //   },
-                //   {
-                //     headers: {
-                //       Authorization: `Bearer ${GPT_API_KEY}`,
-                //     },
-                //   }
-                // );
-                // console.log("GPT API Response:", response.data);  // <-- Log the GPT API response
-                // const summary = response.data.choices[0]?.text?.trim();
-                // ctx.reply(summary || "Unable to summarize.");
-            }
-            else {
-                lastMessage = ((_c = ctx.message) === null || _c === void 0 ? void 0 : _c.text) || "";
-            }
+bot.command("summarize", function (ctx) { return __awaiter(void 0, void 0, void 0, function () {
+    var messages, summary;
+    return __generator(this, function (_a) {
+        switch (_a.label) {
+            case 0: return [4 /*yield*/, fetchRecentMessages(ctx, 50)];
+            case 1:
+                messages = _a.sent();
+                return [4 /*yield*/, getSummary(messages)];
+            case 2:
+                summary = _a.sent();
+                console.log({ summary: summary });
+                ctx.reply(summary);
+                return [2 /*return*/];
         }
-        return [2 /*return*/];
     });
 }); });
+bot.command("summarize_100", function (ctx) { return __awaiter(void 0, void 0, void 0, function () {
+    var messages, summary;
+    return __generator(this, function (_a) {
+        switch (_a.label) {
+            case 0: return [4 /*yield*/, fetchRecentMessages(ctx, 100)];
+            case 1:
+                messages = _a.sent();
+                return [4 /*yield*/, getSummary(messages)];
+            case 2:
+                summary = _a.sent();
+                ctx.reply(summary);
+                return [2 /*return*/];
+        }
+    });
+}); });
+bot.on("text", function (ctx) {
+    var _a;
+    var chatId = (_a = ctx.chat) === null || _a === void 0 ? void 0 : _a.id;
+    // Check if it's a text message
+    if (ctx.message && isTextMessage(ctx.message)) {
+        var messageText = ctx.message.text;
+        if (chatId && messageText) {
+            // Initialize the chat in the recentMessages object if it's not already present
+            if (!recentMessages[chatId]) {
+                recentMessages[chatId] = [];
+            }
+            // Add the message to the recent messages for the chat
+            recentMessages[chatId].push(messageText);
+            // Keep only the last 100 messages for simplicity
+            if (recentMessages[chatId].length > 100) {
+                recentMessages[chatId].shift();
+            }
+        }
+    }
+});
+function fetchRecentMessages(ctx, count) {
+    var _a;
+    return __awaiter(this, void 0, void 0, function () {
+        var chatId;
+        return __generator(this, function (_b) {
+            chatId = (_a = ctx.chat) === null || _a === void 0 ? void 0 : _a.id;
+            if (!chatId || !recentMessages[chatId]) {
+                return [2 /*return*/, []];
+            }
+            return [2 /*return*/, recentMessages[chatId].slice(-count)];
+        });
+    });
+}
+function getSummary(messages) {
+    var _a, _b, _c, _d;
+    return __awaiter(this, void 0, void 0, function () {
+        var systemMessage, userMessage, completion, error_1;
+        return __generator(this, function (_e) {
+            switch (_e.label) {
+                case 0:
+                    systemMessage = {
+                        role: "system",
+                        content: "You are an assistant helping friends catch up in a busy chat group. Your goal is to help friends in this group stay up to date without having to read all the messages.",
+                    };
+                    userMessage = {
+                        role: "user",
+                        content: messages.join("\n"),
+                    };
+                    _e.label = 1;
+                case 1:
+                    _e.trys.push([1, 3, , 4]);
+                    return [4 /*yield*/, openai.createChatCompletion({
+                            model: "gpt-3.5-turbo",
+                            messages: [systemMessage, userMessage],
+                        })];
+                case 2:
+                    completion = _e.sent();
+                    return [2 /*return*/, ((_d = (_c = (_b = (_a = completion.data) === null || _a === void 0 ? void 0 : _a.choices[0]) === null || _b === void 0 ? void 0 : _b.message) === null || _c === void 0 ? void 0 : _c.content) === null || _d === void 0 ? void 0 : _d.trim()) || "Error summarizing the messages."];
+                case 3:
+                    error_1 = _e.sent();
+                    console.error("Error summarizing the messages:", error_1);
+                    return [2 /*return*/, "Error summarizing the messages."];
+                case 4: return [2 /*return*/];
+            }
+        });
+    });
+}
+function isTextMessage(message) {
+    return message && "text" in message;
+}
 bot.launch();
 console.log("Bot is running...");
--- bot.ts
+++ bot.ts
@@ -1,46 +1,94 @@
-
 import { Telegraf, Context } from "telegraf";
-import axios from "axios";
+import { ChatCompletionRequestMessage, ChatCompletionRequestMessageRoleEnum, Configuration, OpenAIApi } from "openai";
 
 const BOT_TOKEN = "6610691916:AAHAVageSJ5M7wSb4PRZozgJmVcCTVWYaQk";
-const GPT_API_ENDPOINT = "https://api.openai.com/v1/engines/davinci/completions";
-const GPT_API_KEY = "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";
+const GPT_API_KEY = "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";
+
+const configuration = new Configuration({
+  apiKey: GPT_API_KEY,
+});
+const openai = new OpenAIApi(configuration);
 
 const bot = new Telegraf(BOT_TOKEN);
 
+// A simple in-memory storage for the recent messages.
+// For a production environment, consider using a more persistent storage solution.
+const recentMessages: { [chatId: number]: string[] } = {};
+
 bot.start((ctx) => ctx.reply("Welcome to the Summary Bot!"));
 
-bot.command('summarize', async (ctx: Context) => {
-    const messages = await fetchRecentMessages(ctx, 50);
-    const summary = await getSummary(messages);
-    ctx.reply(summary);
+bot.command("summarize", async (ctx: Context) => {
+  const messages = await fetchRecentMessages(ctx, 50);
+  const summary = await getSummary(messages);
+  console.log({ summary });
+  ctx.reply(summary);
 });
 
-bot.command('summarize_100', async (ctx: Context) => {
-    const messages = await fetchRecentMessages(ctx, 100);
-    const summary = await getSummary(messages);
-    ctx.reply(summary);
+bot.command("summarize_100", async (ctx: Context) => {
+  const messages = await fetchRecentMessages(ctx, 100);
+  const summary = await getSummary(messages);
+  ctx.reply(summary);
+});
+
+bot.on("text", (ctx: Context) => {
+  const chatId = ctx.chat?.id;
+
+  // Check if it's a text message
+  if (ctx.message && isTextMessage(ctx.message)) {
+    const messageText = ctx.message.text;
+    if (chatId && messageText) {
+      // Initialize the chat in the recentMessages object if it's not already present
+      if (!recentMessages[chatId]) {
+        recentMessages[chatId] = [];
+      }
+
+      // Add the message to the recent messages for the chat
+      recentMessages[chatId].push(messageText);
+
+      // Keep only the last 100 messages for simplicity
+      if (recentMessages[chatId].length > 100) {
+        recentMessages[chatId].shift();
+      }
+    }
+  }
 });
 
 async function fetchRecentMessages(ctx: Context, count: number): Promise<string[]> {
-    // TODO: Fetch the recent messages (up to the specified count) from the Telegram chat.
-    // This is a placeholder and might need adjustments based on the Telegram API's capabilities and the Telegraf library.
-    const messages: string[] = [];
-    return messages;
+  const chatId = ctx.chat?.id;
+  if (!chatId || !recentMessages[chatId]) {
+    return [];
+  }
+
+  return recentMessages[chatId].slice(-count);
 }
 
 async function getSummary(messages: string[]): Promise<string> {
-    // Send the messages to the OpenAI API for summarization.
-    const response = await axios.post(GPT_API_ENDPOINT, {
-        prompt: messages.join("\n"),
-        max_tokens: 150
-    }, {
-        headers: {
-            'Authorization': `Bearer ${GPT_API_KEY}`
-        }
+  const systemMessage: ChatCompletionRequestMessage = {
+    role: "system" as ChatCompletionRequestMessageRoleEnum,
+    content:
+      "You are an assistant helping friends catch up in a busy chat group. Your goal is to help friends in this group stay up to date without having to read all the messages.",
+  };
+
+  const userMessage: ChatCompletionRequestMessage = {
+    role: "user" as ChatCompletionRequestMessageRoleEnum,
+    content: messages.join("\n"),
+  };
+
+  try {
+    const completion = await openai.createChatCompletion({
+      model: "gpt-3.5-turbo",
+      messages: [systemMessage, userMessage],
     });
 
-    return response.data.choices[0].text.trim();
+    return completion.data?.choices[0]?.message?.content?.trim() || "Error summarizing the messages.";
+  } catch (error) {
+    console.error("Error summarizing the messages:", error);
+    return "Error summarizing the messages.";
+  }
+}
+
+function isTextMessage(message: any): message is { text: string } {
+  return message && "text" in message;
 }
 
 bot.launch();
