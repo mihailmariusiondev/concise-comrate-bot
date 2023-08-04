1c7718128425af9e54afdd215c15284fb06ba176
Marius <marius.mihail@enzo.es>
2023-08-05 00:38:09 +0200

    add lang detector

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -22,6 +22,7 @@ export async function getSummaryForChat(chatId: number): Promise<string> {
   const systemMessage = `You are an assistant helping friends catch up in a busy chat group. Your goal is to help friends in this group stay up to date without having to read all the messages.
     You will receive a recent conversation that happened in the group. Respond immediately with a short and concise summary of the conversation, capturing key details and significant events.
     The summary should have the following characteristics:
+    - NEVER reference message IDs (e.g., #360).
     - Should be in ${language} language
     - Should have a tone that is similar to the conversation, act like you are part of the group
     - Use 3 sentences or less
