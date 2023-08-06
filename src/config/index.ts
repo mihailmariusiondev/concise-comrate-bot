0e9c8609fc85a5a99f7d94ffa51fcf9c1961f3c2
Marius <marius.mihail@enzo.es>
2023-08-06 19:28:42 +0200

    botStatus

--- src/config/index.ts
+++ src/config/index.ts
@@ -15,9 +15,7 @@ export const BOT_DISABLE_COMMAND = "/disable";
 // Bot messages
 export const START_MESSAGE = "¡Paisha crack! Vamos a resumir tus chats de mierda";
 export const BOT_ENABLED_MESSAGE = "Bot activado";
-// export const BOT_ALREADY_ENABLED_MESSAGE = "El bot ya está activado";
 export const BOT_DISABLED_MESSAGE = "Bot desactivado";
-// export const BOT_ALREADY_DISABLED_MESSAGE = "El bot ya está desactivado";
 export const NOT_ENOUGH_MESSAGES_REPLY = "No hay suficientes mensajes para resumir. Necesito al menos 5 mensajes";
 export const COOLDOWN_MESSAGE = "Machooo, espérate un poco antes de volver a usar el comando";
 export const ERROR_SUMMARIZING = "Ya la hemos liao... Error al resumir los mensajes";
